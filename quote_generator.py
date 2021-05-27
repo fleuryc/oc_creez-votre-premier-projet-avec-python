44324c7b0838503f793c12209bdc2643fe024251
Clement Fleury <clement.fleury@pm.me>
2021-05-27 16:09:32 +0200

    fix linting

--- quote_generator.py
+++ quote_generator.py
@@ -1,19 +1,26 @@
-import random, json
+import random
+import json
+
 
 def read_values_from_json(file):
     # load data
     with open(file) as f:
         return json.load(f)
 
+
 characters = read_values_from_json('data/characters.json')
 quotes = read_values_from_json('data/quotes.json')
+
 
 def get_random_item(list):
     # pick a random item from list
     return list[random.randint(0, len(list)-1)]
 
-# While the user doesn't press "B", just display a random quote from a random character.
-while input("Tapez 'Entree' pour obtenir une nouvelle citation, ou 'B' pour sortir.\n") != "B":
-	print('{character} says : "{quote}"\n'.format(character = get_random_item(characters).capitalize(), quote=get_random_item(quotes)))
+
+while input("Type 'Enter' for a new quote, or 'B' to quit.\n") != "B":
+    print(
+        '{character} says : "{quote}"\n'.format(
+            character=get_random_item(characters).capitalize(),
+            quote=get_random_item(quotes)))
 
 print("Bye !")
